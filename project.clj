0e937d7c041a7330ecdf47dfac8b1e2edf6fd37d
Jozef Wagner <jozef.wagner@gmail.com>
2015-06-10 21:09:38 +0200

    updatE

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject crispin "0.1.0-SNAPSHOT"
+(defproject crispin "0.1.0"
   :description "Dunaj library for managing project-wide configuration."
   :url "https://github.com/dunaj-project/crispin"
   :license {:name "Eclipse Public License"
+            :distribution :repo
+            :comments "same as Clojure"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dunaj/dunaj "0.6.0-SNAPSHOT"]])
+  :scm {:name "git" :url "https://github.com/dunaj-project/crispin"}
+  :signing {:gpg-key "6A72CBE2"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :profiles {:dev {:dependencies [[org.dunaj/dunaj "0.6.0-SNAPSHOT"]]}})
