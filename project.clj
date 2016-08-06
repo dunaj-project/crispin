996f56c27098d53876ef3093d882639b2e1ba36f
Jozef Wagner <jozef.wagner@gmail.com>
2016-08-06 13:51:59 +0200

    Next version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crispin "0.3.1"
+(defproject crispin "0.3.2-SNAPSHOT"
   :description "A library for managing project-wide configuration."
   :url "https://github.com/dunaj-project/crispin"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
             :comments "same as Clojure"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/dunaj-project/crispin"}
-  :signing {:gpg-key "487FA463"}
+  :signing {:gpg-key "AFA4E115"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[cheshire "5.6.3"]
                  [org.clojure/clojure "1.8.0"]]
